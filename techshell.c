f00213f4ab876536f03b3e9c70036031043e3569
ZacharyTrux <ztruxillo@gmail.com>
2025-02-13 12:39:30 -0600

    sd

--- techshell.c
+++ techshell.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+char* CommandPrompt(); // recieve input
+struct ShellCommand ParseCommandLine(char* input); // figure out what the command is 
+void ExecuteCommand(struct ShellCommand command); // execute the command 
+
 int main(){
-    
+    char* input;
+    struct ShellCommand command;
+
+    for(;;){
+        input = CommandPrompt();
+        command = ParseCommandLine(input);
+        ExecuteCommand(command);
+    }
+
+    exit(0);
 }
