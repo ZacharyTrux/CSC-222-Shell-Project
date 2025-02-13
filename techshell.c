8a694535824473179c2cab9542cb617dabc955c7
ZacharyTrux <ztruxillo@gmail.com>
2025-02-13 13:32:59 -0600

    set up the commmand line function

--- techshell.c
+++ techshell.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define DEBUG
 
 char* CommandPrompt(); // recieve input
-struct ShellCommand ParseCommandLine(char* input); // figure out what the command is 
-void ExecuteCommand(struct ShellCommand command); // execute the command 
+//struct ShellCommand ParseCommandLine(char* input); // figure out what the command is 
+//void ExecuteCommand(struct ShellCommand command); // execute the command 
 
 int main(){
     char* input;
-    struct ShellCommand command;
+    //struct ShellCommand command;
 
     for(;;){
         input = CommandPrompt();
-        command = ParseCommandLine(input);
-        ExecuteCommand(command);
+        //command = ParseCommandLine(input);
+        //ExecuteCommand(command);
     }
 
     exit(0);
+}
+
+char* CommandPrompt(){
+    char buffer[PATH_MAX + 1];
+    char *cwd = getcwd(buffer, PATH_MAX + 1); // gets the working directory
+    printf("%s$ ", cwd);
+
+
+
+    char *input;
+    input = (char*)malloc(50 * sizeof(char));
+    scanf("%s", input);
+    //printf("\n");
+    
+    return input;
 }
