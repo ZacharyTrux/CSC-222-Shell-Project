a0212bffadada3a48200a9b5a8733b76b8208ee8
ZacharyTrux <ztruxillo@gmail.com>
2025-02-15 13:20:28 -0600

    Progress

--- techshell.c
+++ techshell.c
@@ -11,7 +11,7 @@
 
 char* CommandPrompt(); // recieve input
 struct ShellCommand ParseCommandLine(char* input); // figure out what the command is 
-//void ExecuteCommand(struct ShellCommand command); // execute the command 
+void ExecuteCommand(struct ShellCommand command); // execute the command 
 
 int main(){
     char* input;
@@ -20,20 +20,43 @@ int main(){
     for(;;){
         input = CommandPrompt();
         command = ParseCommandLine(input);
-        printf(command);
-        //ExecuteCommand(command);
+        if(DEBUG){printf("input: %s, command: %s\n", input, command);}
+        ExecuteCommand(command);
     }
 
     exit(0);
 }
 
 struct ShellCommand {
-    char command[MAX_COMMAND_LENGTH];
-    char args[MAX_ARGS][MAX_COMMAND_LENGTH];
+    char *command[MAX_COMMAND_LENGTH];
+    char *args[MAX_ARGS][MAX_COMMAND_LENGTH];
     int num_args;
 }
-ShellCommand ParseCommandLine(char *input){
-    
+struct ShellCommand ParseCommandLine(char *input){
+    struct ShellCommand commandLine;
+    commandLine.num_args = 0;
+    commandLine.command = input[0];
+
+    int size = sizeof(input) / sizeof(char*);
+
+    for(int i = 1; i < size; i++){
+        commandLine.args[i][] = input[i];
+        for (int j = i; j <= i; j++){
+            commandLine.args[i][j] = sizeof(input[i]) / sizeof(char*);  
+        }
+        commandLine.num_args++;
+    }
+
+    return commandLine;
+}
+
+void ExecuteCommand(struct ShellCommand commandLine){
+    pid_t p = fork();
+    if(p < 0){
+        perror("Fork Failed");
+        exit(1);
+    }
+    execvp(commandLine.command, commandLine.args);
 }
 
 char* CommandPrompt(){
